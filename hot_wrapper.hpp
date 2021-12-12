e8d66918350973a33c0e5cebd199db9583d7917d
Baotong LU <lubaotong22@gmail.com>
2021-12-12 15:48:41 +0800

    continue debugging

--- hot_wrapper.hpp
+++ hot_wrapper.hpp
@@ -16,18 +16,12 @@ public:
   }
 
   void bulk_load(const V bulk_arr[], int num) {
-    char path[1024];
-    for (int i = 0; i < 10; ++i) {
-      memcpy(path, bulk_arr[i].first->key, bulk_arr[i].first->length);
-      path[bulk_arr[i].first->length] = '\0';
-      printf("%s\n", path);
-    }
-
     for (int i = 0; i < num; ++i) {
       char fixed_string_key[256] = {0};
       memcpy(fixed_string_key, bulk_arr[i].first->key,
              bulk_arr[i].first->length);
       fixed_string_key[bulk_arr[i].first->length] = '\0';
+      printf("%s\n", fixed_string_key);
       auto val =
           new std::pair<const char *, P>(fixed_string_key, bulk_arr[i].second);
       my_tree->insert(val);
@@ -50,14 +44,10 @@ public:
 
   bool search(const T &key, P *payload) const {
     if constexpr (std::is_pointer_v<T>) {
-      char path[1024];
-      memcpy(path, key->key, key->length);
-      path[key->length] = '\0';
-      printf("%s\n", path);
-
       char fixed_string_key[256] = {0};
       memcpy(fixed_string_key, key->key, key->length);
       fixed_string_key[key->length] = '\0';
+      printf("%s\n", fixed_string_key);
       auto result = my_tree->lookup(fixed_string_key);
       if (result.mIsValid) {
         *payload = result.mValue->second;
